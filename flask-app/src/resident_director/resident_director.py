18fea50c04df4512df7218f4f4ea89ebf0a9d015
Ruhan Xia <xia.ruh@northeastern.edu>
2023-04-15 13:00:43 -0400

    blueprint

--- flask-app/src/resident_director/resident_director.py
+++ flask-app/src/resident_director/resident_director.py
@@ -3,10 +3,10 @@ import json
 from src import db
 
 
-products = Blueprint('products', __name__)
+resident_director = Blueprint('resident_director', __name__)
 
 # Get all the products from the database
-@products.route('/products', methods=['GET'])
+@resident_director.route('/resident_director', methods=['GET'])
 def get_products():
     # get a cursor object from the database
     cursor = db.get_db().cursor()
@@ -32,7 +32,7 @@ def get_products():
     return jsonify(json_data)
 
 # get the top 5 products from the database
-@products.route('/mostExpensive')
+@resident_director.route('/mostExpensive')
 def get_most_pop_products():
     cursor = db.get_db().cursor()
     query = '''
