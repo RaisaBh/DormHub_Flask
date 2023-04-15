b0c7dc74b135859779610b5d25cbfb2a1c71f168
ananyar807 <ananyar807@gmail.com>
2023-04-15 17:10:48 -0400

    added my routes

--- flask-app/src/resident_director/resident_director.py
+++ flask-app/src/resident_director/resident_director.py
@@ -58,3 +58,65 @@ def get_most_pop_products():
         json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
+
+
+ # Update the start time for an RD
+@resident_director.route('/WeeklySchedule/<ra_id>/<start_time>', methods=['PUT'])
+def update_start_time(ra_id, start_time):
+    cursor = db.get_db().cursor()
+    # {0}'.format(userID)
+    query = '''
+        UPDATE WeeklySchedule
+        SET start_time = {0}
+        WHERE ra_id = {0}
+    '''.format(ra_id, start_time)
+    cursor.execute(query)
+
+#updates the ra for a given student
+@resident_director.route('/Residents/<student_id>/<ra_id>', methods=['PUT'])
+def update_ra(student_id, ra_id):
+    cursor = db.get_db().cursor()
+    query = '''
+        UPDATE Residents
+        SET ra_id = {0}
+        WHERE student_id = {0}
+    '''.format(ra_id, student_id)
+    cursor.execute(query)
+
+#deletes the ra for a given weekly schedule
+@resident_director.route('/WeeklySchedule/<ra_id>', methods=['DELETE'])
+def update_ra(ra_id):
+    cursor = db.get_db().cursor()
+    query = '''
+        DELETE FROM WeeklySchedule
+        WHERE ra_id = {0}
+    '''.format(ra_id)
+    cursor.execute(query)
+
+#deletes the a student from the residents (they might move out for ex.)
+@resident_director.route('/Residents/<student_id>', methods=['DELETE'])
+def update_ra(student_id):
+    cursor = db.get_db().cursor()
+    query = '''
+        DELETE FROM Residents
+        WHERE student_id = {0}
+    '''.format(student_id)
+    cursor.execute(query)
+
+# Get all the start times for a resident director 
+@resident_director.route('/getStartTimes', methods=['GET'])
+def get_products():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute('SELECT start_time FROM WeeklySchedule')
+
+# Get all the years for Residents
+@resident_director.route('/getResidentYears', methods=['GET'])
+def get_products():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute('SELECT year FROM Residents')
