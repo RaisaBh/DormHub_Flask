289947e62fc4aa28c9bf43ad17a8e15822885f87
Ruhan Xia <xia.ruh@northeastern.edu>
2023-04-15 14:41:11 -0400

    fix

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -31,8 +31,8 @@ def create_app():
 
     # Import the various routes
     from src.views import views
-    from src.customers.customers import customers
-    from src.products.products  import products
+    from src.residents.residents import residents
+    from src.resident_director.resident_director  import resident_director
 
     # Register the routes that we just imported so they can be properly handled
     app.register_blueprint(views,       url_prefix='/v')
