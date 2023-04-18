0abf45257ceb599640afd340c4e9e6ca8f118079
Ruhan Xia <xia.ruh@northeastern.edu>
2023-04-18 13:18:12 -0400

    add_new_route

--- flask-app/src/residents/residents.py
+++ flask-app/src/residents/residents.py
@@ -198,3 +198,18 @@ def get_all_interests():
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+# Route10-Get: Get all of the songs
+@residents.route('/all_spotifyPlaylist', methods=['GET'])
+def get_all_songs():
+    cursor = db.get_db().cursor()
+    cursor.execute('select * from SpotifyPlaylist')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
